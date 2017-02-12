03ebd14947899ea20af89ceb09caac6fff637cd8
Zachary Bennett <zbennett10@hotmail.com>
2017-02-12 13:45:04 -0500

    added some basic graphics and functionality

--- app.js
+++ app.js
@@ -1,159 +1,127 @@
+var sevenSummits = [{
+            name: "Mt. Everest",
+            longitude: 86.925,
+            latitude: 27.9878
+        }, {
+            name: "Mt. Aconcagua",
+            longitude: -70.0109,
+            latitude: -32.6532
+        }, {
+            name: "Mt. Denali",
+            longitude: -151.0070,
+            latitude: 63.0692
+        }, {
+            name: "Mt. Kilimanjaro",
+            longitude: 37.3556,
+            latitude: -3.0674
+        }, {
+            name: "Mt. Elbrus",
+            longitude: 42.4453,
+            latitude: 43.3499
+        }, {
+            name: "Mt. Vinson",
+            longitude: -85.2135,
+            latitude: -78.6341
+        }, {
+            name:"Mt. Kosciuszko",
+            longitude: 148.2636,
+            latitude: -36.4559
+        }
+    ];
+
 
 //load dojo module containing arcgis api
-require(["esri/Map","esri/views/SceneView", "esri/layers/GraphicsLayer",
-        "esri/Graphic", "esri/geometry/Point", "esri/geometry/Polyline",
-        "esri/geometry/Polygon", "esri/symbols/SimpleMarkerSymbol",
-        "esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol", 
-        "esri/geometry/Circle", "esri/geometry/support/webMercatorUtils", "dojo/domReady!"], 
-    function(Map, SceneView, GraphicsLayer, Graphic, Point,
-            Polyline, Polygon, SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol,
-            Circle, webMercatorUtils){
-        let startLat;
-        let startLon;
-        let endLat;
-        let endLon;
-        
+require(["esri/Map","esri/views/MapView",
+        "esri/layers/GraphicsLayer", "esri/Graphic",
+        "esri/geometry/Polyline", "esri/symbols/SimpleLineSymbol",
+        "esri/geometry/Point", "esri/symbols/SimpleMarkerSymbol",
+        "esri/geometry/Polygon", "esri/symbols/SimpleFillSymbol",
+        "esri/geometry/support/webMercatorUtils", "esri/symbols/TextSymbol",
+        "esri/layers/FeatureLayer",
+         "dojo/domReady!"], 
+    function(Map, MapView, GraphicsLayer, Graphic,
+            Polyline, SimpleLineSymbol, Point, SimpleMarkerSymbol,
+            Polygon, SimpleFillSymbol, webMercatorUtils, TextSymbol,
+            FeatureLayer){
+
         var map = new Map({
-            basemap: "streets",
-            ground: "world-elevation"
+            basemap: "hybrid"
         });
-        var view = new SceneView({
-            container: "viewDiv",     
-            map: map,                 
-            scale: 50000000    
-            // camera: {
-            //     position: {
-            //         x: -0.2,
-            //         y: 51.4,
-            //         z: 1266.7
-            //     }
-            // heading: 0.34,
-            // tilt: 83
-            //} 
-        });
-        
-        //create graphics layer
-        var graphicsLayer = new GraphicsLayer();
-        map.add(graphicsLayer);
-        
 
-        //add marker to map -- break these into one function call
-       //createMapMarker(-0.178, 51.48791);
         
-        
-        
-        //fix markerline and rings not showing up
-        //assign start and end values to a polyline connecting points
+        var view = new MapView({
+            container: "viewDiv",  
+            map: map,
+            center: [-.01, 50],
+            zoom: 3     
+        });
+
+        //when map is loaded populate the seven summits
+        view.then(() => {
+            console.log("Map loaded");
+            var summitCoords = sevenSummits.map(summit => {
+                return createMapPoint(summit.latitude, summit.longitude, summit.name);
+            });
+            
+            view.graphics.addMany(summitCoords);
+            view.popup.features = summitCoords;
+            view.popup.dockEnabled = true; //dock popup to upper right of map
+            view.popup.visible = true;
+        });
+
+
+        //handle user submit coordinates to create map point
         document.getElementById('searchSubmit').onclick = function(event) {
-            event.preventDefault();
-            var startLat = document.getElementById('startLat').value;
-            var startLon = document.getElementById('startLon').value;
-            var endLat = document.getElementById('endLat').value;
-            var endLon = document.getElementById('endLon').value;
-            createMapMarker(startLon, startLat);
-            createMapMarker(endLon, endLat);
+            var lat = document.getElementById('startLat').value;
+            var lon = document.getElementById('startLon').value;
+            var point = createMapPoint(lat, lon);
+             view.graphics.add(point);
+                view.goTo({
+                    target: point,
+                    zoom: 13
+                }, {duration: 5000});
         }
         
 
-        function createMapMarker(lon, lat) {
-            createMarkerPoint(lon, lat);
-            createMarkerLine(lon, lat);
-            createMarkerRings(lon, lat);
-        }
-
-        //create map point
-        function createMarkerPoint(lon, lat) {
-            var point = new Point({
-                    x: lon,
-                    y: lat,
-                    z: 1010
-            }),
-
-            pointSymbol = new SimpleMarkerSymbol({
-                color: [226, 40, 40],
-                outline: {
-                    color: [255, 255, 255],
-                    width: 5
-                }
+ 
+        //create a map point
+        function createMapPoint(lat, lon, label) {
+            console.log("creating point");
+            var point = new Point ({
+                longitude: lon,
+                latitude: lat
             });
 
+            var pointSymbol = new SimpleMarkerSymbol({
+                    color: [226, 40, 40],
+                    outline: {
+                        color: [255, 255, 255],
+                        width: 5
+                    }
+            });
+
+            var pointAtt = {
+                Name: label,
+                Coords: `${lat}, ${lon}`
+            }
+   
             var pointGraphic = new Graphic({
-                geometry: point,
-                symbol: pointSymbol
+                    geometry: point,
+                    symbol: pointSymbol,
+                    attributes: pointAtt,
+                    popupTemplate: { // autocasts as new PopupTemplate()
+                    title: "{Name}",
+                    content: [{
+                        type: "fields",
+                        fieldInfos: [{
+                        fieldName: "Name"
+                        }, {
+                        fieldName: "Coords"
+                        }]
+                    }]
+                    }
             });
-
-            graphicsLayer.add(pointGraphic);
-        }
-
-        //create map line
-        function createMarkerLine(lon, lat) {
-
-            var markerLine = new Polyline([
-                [lon, lat, 0],
-                [lon, lat, 1000]
-            ]),
-
-            markerLineSymbol = new SimpleLineSymbol({
-                color: [226, 40, 40],
-                width: 10
-            });
-
-            var markerLineGraphic = new Graphic({
-                geometry: markerLine,
-                symbol: markerLineSymbol
-            });
-
-            graphicsLayer.add(markerLineGraphic);
-        }
-       
-       //create map rings  -0.178, 51.48791
-        function createMarkerRings(lon, lat) {
-            var xOffsetTop = lon - .006;
-            var xOffsetBottom = lon + .006;
-            var yOffsetTop = lat - .004;
-            var yOffsetBottom = lat + .003;
-            var markerRings = new Polygon({
-                rings: [
-                    [
-                        [xOffsetTop, yOffsetTop, 400],
-                        [xOffsetTop, yOffsetBottom, 500],
-                        [xOffsetBottom, yOffsetBottom, 500],
-                        [xOffsetBottom, yOffsetTop, 400],
-                        [xOffsetTop, yOffsetTop, 400]
-                    ], 
-                    [
-                        [xOffsetTop, yOffsetTop, 200],
-                        [xOffsetTop, yOffsetBottom, 300],
-                        [xOffsetBottom, yOffsetBottom, 300],
-                        [xOffsetBottom, yOffsetTop, 200],
-                        [xOffsetTop, yOffsetTop, 200]
-                    ],
-                    [
-                        [xOffsetTop, yOffsetTop, 600],
-                        [xOffsetTop, yOffsetBottom, 700],
-                        [xOffsetBottom, yOffsetBottom, 700],
-                        [xOffsetBottom, yOffsetTop, 600],
-                        [xOffsetTop, yOffsetTop, 600]
-                    ]
-                ]
-            }),
-
-            fillSymbol = new SimpleFillSymbol({
-                color: [227, 40, 79, 0.8],
-                outline: { // autocasts as new SimpleLineSymbol()
-                color: [255, 255, 255],
-                width: 1
-                }
-            });
-
-            var markerRingsGraphic = new Graphic({
-                geometry: markerRings,
-                symbol: fillSymbol
-            });
-
-            graphicsLayer.add(markerRingsGraphic);
-        }
-
-    
+            return pointGraphic;
+         }
 });
 
